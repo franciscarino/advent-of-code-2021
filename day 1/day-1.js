d86416777e86c136952a202fb30997d5cf985e38
Francis Carino <fgcarino@gmail.com>
2022-11-19 21:57:03 -0800

    complete day 1 pt 2

--- day 1/day-1.js
+++ day 1/day-1.js
@@ -13,4 +13,16 @@ function depthMeasurement(data) {
 }
 
 
+//Part 2
+function countIncreases(array) {
+  let numIncreases = 0;
+
+  for (let i = 3; i < array.length; i++) {
+    let current = array[i - 2] + array[i - 1] + array[i];
+    let previous = array[i - 3] + array[i - 2] + array[i - 1];
+
+    if (current > previous) numIncreases++;
+  }
+  return numIncreases;
+}
 
